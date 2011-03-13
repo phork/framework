52667609ff3582808f7bcf96b53df8ceb40bbf50
Elenor <github@elenor.net>
2011-03-13 09:45:29 -0700

    Improved save helpers in the example model

--- sites/standard/models/ExampleModel.class.php
+++ sites/standard/models/ExampleModel.class.php
@@ -7,8 +7,14 @@
 	 * Used to add, edit, delete and load the example records
 	 * from the database using the database model.
 	 *
+	 * Copyright 2006-2011, Phork Labs. (http://phorklabs.com)
+	 *
+	 * Licensed under The MIT License
+	 * Redistributions of files must retain the above copyright notice.
+	 *
 	 * @author Elenor Collings <elenor@phork.org>
-	 * @package wemo-ocean
+	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+	 * @package phork
 	 * @subpackage models
 	 */
 	class ExampleModel extends CoreDatabaseModel {
@@ -18,6 +24,8 @@
 		
 		protected $arrInsertCols = array('userid', 'name', 'created', 'updated');
 		protected $arrUpdateCols = array('name', 'updated');
+		
+		protected $blnSaveHelpers;
 		
 		
 		/**
@@ -145,12 +153,15 @@
 		 * @access protected
 		 */
 		protected function addSaveHelpers() {
-			if (empty($this->arrConfig['NoSaveHelpers'])) {
-				if (!array_key_exists('cache-bust-save', $this->arrHelpers)) {
-					if (AppLoader::includeExtension('helpers/', 'ModelCache')) {
-						$this->appendHelper('cache-bust-save', 'ModelCache');
-						$this->initHelper('cache-bust-save', array('postSave'));
+			if (!$this->blnSaveHelpers) {
+				if (empty($this->arrConfig['NoSaveHelpers'])) {
+					if (!array_key_exists('cache-bust-save', $this->arrHelpers)) {
+						if (AppLoader::includeExtension('helpers/', 'ModelCache')) {
+							$this->appendHelper('cache-bust-save', 'ModelCache');
+							$this->initHelper('cache-bust-save', array('postSave'));
+						}
 					}
+					$this->blnSaveHelpers = true;
 				}
 			}
 		}
@@ -270,5 +281,24 @@
 		public function save($blnForceInsert = false) {
 			$this->addSaveHelpers();
 			return parent::save($blnForceInsert);
+		}
+		
+		
+		/*****************************************/
+		/**     MAGIC METHODS                   **/
+		/*****************************************/
+		
+		
+		/**
+		 * Method called when the object is cloned. Resets
+		 * the event key and helpers and then calls init()
+		 * to re-initialize them with a new event key.
+		 * Also clears the blnSaveHelpers flag.
+		 *
+		 * @access public
+		 */
+		public function __clone() {
+			parent::__clone();
+			$this->blnSaveHelpers = false;
 		}
 	}
